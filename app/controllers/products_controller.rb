afbc76d10a3da29f5314440b995de29572f2bcaa
Bogdan <bogdan1281993@gmail.com>
2018-09-02 11:26:07 +0300

    fixes from the pull req

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,7 @@ class ProductsController < ApplicationController
 	end
 	
 	def create
-		@product = Product.new(params.require(:product).permit(:title, :description, :price, :photo))
+		@product = Product.new(product_params)
  
 		if @product.save
 			redirect_to @product
