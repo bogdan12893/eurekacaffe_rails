b6a664b16567d8b9a729a089de971fcbe87707db
Bogdan <bogdan1281993@gmail.com>
2018-09-17 09:32:52 +0300

    login/logout implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_13_133637) do
+ActiveRecord::Schema.define(version: 2018_09_14_113112) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2018_09_13_133637) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
